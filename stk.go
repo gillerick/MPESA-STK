f3dced8987b34cd6b82f2a2c3f52eb6b2701a205
Gill Erick <37691932+gillerick@users.noreply.github.com>
2021-02-09 11:57:01 +0300

    Create stk.go

---
+++ stk.go
@@ -0,0 +1,11 @@
+package main
+
+const CONSUMER_KEY = "consumerkey"
+const CONSUMER_SECRET = "consumser_secret"
+const API_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+
+
+
+func main(){
+	
+}
